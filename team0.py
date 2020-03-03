f3b4f187bd06095550a3d9738f0d2f677d68977d
EHAYMOND <61420394+EHAYMOND@users.noreply.github.com>
2020-03-03 08:30:56 -0600

    Update team0
    updated strategy

--- team0.py
+++ team0.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Eli_Is_The_Goat' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Eli_Is_Goat' # Only 10 chars displayed.
+strategy_name = 'No Snitches'
+strategy_description = 'No one tells on one another'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
